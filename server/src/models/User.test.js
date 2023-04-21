5c8b604661f17f3c4e4dc9d9d2ea46c3ec2d78c6
nimonian <nimon@hotmail.co.uk>
2023-04-21 19:18:55 +0100

    add comments

--- server/src/models/User.test.js
+++ server/src/models/User.test.js
@@ -2,13 +2,16 @@ const { describe, it, expect, beforeAll, afterAll } = require('@jest/globals')
 const { User } = require('.')
 const db = require('../db/config')
 
+// define in global scope
 let user
 
+// clear db and create new user before tests
 beforeAll(async () => {
   await db.sync({ force: true })
   user = await User.create({ username: 'gandalf' })
 })
 
+// clear db after tests
 afterAll(async () => await db.sync({ force: true }))
 
 describe('User', () => {
